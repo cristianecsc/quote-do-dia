7712c79a21599ced76b92ed30587a055956d666a
cristianecsc <cristianecarolinaestd@gmail.com>
2026-07-18 11:56:55 -0300

    Add motivational quotes to script.js

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-
+[
+  {
+    "text": "A mente que se abre a uma nova ideia jamais volta ao seu tamanho original.",
+    "author": "Albert Einstein"
+  },
+  {
+    "text": "Não é na tranquilidade que o caráter é formado, mas nos desafios que escolhemos enfrentar.",
+    "author": ""
+  },
+  {
+    "text": "Um livro é um sonho que você segura nas mãos.",
+    "author": "Neil Gaiman"
+  },
+  {
+    "text": "A disciplina é a ponte entre objetivos e realizações.",
+    "author": "Jim Rohn"
+  },
+  {
+    "text": "O conhecimento é a única riqueza que aumenta quando é compartilhada.",
+    "author": ""
+  },
+  {
+    "text": "A excelência não é um ato, mas um hábito.",
+    "author": "Aristóteles"
+  },
+  {
+    "text": "Grandes coisas nunca são feitas por uma única pessoa; elas são feitas por uma equipe.",
+    "author": "Steve Jobs"
+  },
+  {
+    "text": "A jornada de mil quilômetros começa com um único passo.",
+    "author": "Lao Tsé"
+  },
+  {
+    "text": "O segredo do progresso é começar.",
+    "author": "Mark Twain"
+  },
+  {
+    "text": "Aprender é a única coisa que a mente nunca se cansa, nunca teme e nunca se arrepende.",
+    "author": "Leonardo da Vinci"
+  }
+]
